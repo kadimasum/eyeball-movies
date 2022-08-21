e6f00c4a147053e02925bf109fd51a8db8ad952b
AugustineKadima <kadimasam@gmail.com>
2022-08-21 03:32:42 +0300

    Add footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,14 @@ export default function Footer(){
     return (
         <div className="footer_container">
 
+            <div className="footer_wrapper">
+                
+                <div className='logo_and_search'>
+                    <h2>EyeballMovies</h2>
+                    <p> &copy; Kadima 2022</p>
+                </div>
+            </div>
+
         </div>
     )
 }
